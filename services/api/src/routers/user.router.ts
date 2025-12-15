28650e6852a3a2ee7bd0dd2fb0f03f5a1a226be3
Harsh Sandhu <hsandhu@speareducation.com>
2025-12-15 23:10:54 +0530

    fix: update schema

--- services/api/src/routers/user.router.ts
+++ services/api/src/routers/user.router.ts
@@ -21,13 +21,17 @@ export const userRouter = t.router({
   getByCognitoId: t.procedure
     .input(z.object({ cognitoId: z.string() }))
     .query(async ({ ctx, input }): Promise<User | null> => {
-      const users = await ctx.models.User.find({ cognitoId: input.cognitoId }, { limit: 1 });
+      const users = await ctx.models.User.find({ cognitoId: input.cognitoId }, { index: 'gsi2', limit: 1 });
       return users.length > 0 ? (users[0] as User) : null;
     }),
 
   create: t.procedure.input(CreateUserSchema).mutation(async ({ ctx, input }): Promise<User> => {
+    const now = Date.now(); // Get numeric timestamp
     const user = await ctx.models.User.create({
+      id: Bun.randomUUIDv7(),
       ...input,
+      createdAt: now,
+      updatedAt: now,
     });
     return user as User;
   }),
